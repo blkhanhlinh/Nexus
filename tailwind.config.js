3e14e1cba558e03df4d8bf99d88b19b9d5d02be2
blkhanhlinh <linhbui8103@gmail.com>
2024-05-13 10:31:38 +0700

    config and setup project 😈

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,17 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require('tailwindcss/defaultTheme')
-
 export default {
-  content: ['./src/**/*.{html,js,ts,Vue}'],
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
   theme: {
-    color: {
+    colors: {
       'text': {
         'main': '#F3F3F3',
         'dim': '#76808C',
       },
       'bg': {
-        'main': '0E141B',
+        'main': '#0E141B',
         'highlight': '#1E2329',
         'hover': '#313843',
         'secondary': '#14344B',
@@ -22,40 +23,33 @@ export default {
         'green': '#A1CD44',
         'red': '#CD5444',
         'yellow': '#C1B15F',
-      }
-    },
-    extend: {
-      fontFamily: {
-        sans: [
-          'Motiva Sans',
-          ...defaultTheme.fontFamily.sans,
-        ]
       },
-      fontSize: {
-        'heading': {
-          'lg': ['1.625rem', {
-            fontWeight: '700',
-          }],
-          'md': ['1.375rem', {
-            fontWeight: '700',
-          }],
-          'sm': ['1.125rem', {
-            fontWeight: '700',
-          }],
-        },
-        'body': {
-          'lg': ['1rem', {
-            fontWeight: '400',
-          }],
-          'md': ['0.875rem', {
-            fontWeight: '400',
-          }],
-          'sm': ['0.75rem', {
-            fontWeight: '400',
-          }]
-        }
-      }
+      'blue': '#1A9FFF',
+      'blueHi': '#00BBFF',
+      'green': '#5BA32B',
+      'greenHi': '#59BF40',
+      'orange': '#E35E1C',
+      'red': '#D94126',
+      'redHi': '#EE563B',
+      'dustyBlue': '#417a9b',
+      'lightBlue': '#B3DFFF',
+      'yellow': '#FFC82C',
+      'chalkyBlue': '#66C0F4',
     },
+    borderRadius: {
+      DEFAULT: '3px',
+      'lg': '5px',
+    },
+    fontSize: {
+      xs: '0.625rem',
+      sm: '0.75rem',
+      base: '0.875rem',
+      lg: '1rem',
+      xl: '1.125rem',
+      '2xl': '1.375rem',
+      '3xl': '1.625rem',
+    },
+    extend: {},
   },
   plugins: [],
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,23 @@
 import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'url'
+import { resolve, dirname } from 'path'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: [{ find: "@", replacement: resolve(__dirname, "./src") }]
+  },
   server: {
     port: 8080
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/styles/variables.scss";`
+      }
+    }
   }
 })
