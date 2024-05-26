07d4d32746dba35a152772097d03d121b68770e8
blkhanhlinh <linhbui8103@gmail.com>
2024-05-26 15:00:16 +0700

    feat: add layout and custom Pacman loader, also adjust app to make it auto navigate to proper path 🐣

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
-<script setup lang="ts">
-</script>
-
 <template>
-
+  <router-view />
 </template>
 
-<style scoped>
-</style>
+<script lang="ts" setup></script>
+
+<style scoped></style>
---
+++ src/components/Layout.vue
@@ -0,0 +1,23 @@
+<template>
+    <Navbar :show-subnav="!isLogin" />
+    <main v-bind="$attrs">
+        <div :class="
+            isLogin || isFull ? '' : 'container mx-auto pb-20 px-2'
+        ">
+            <slot></slot>
+        </div>
+    </main>
+    <Footer />
+</template>
+
+<script setup lang="ts">
+import Navbar from "@/components/Navbar.vue";
+import Footer from "@/components/Footer.vue";
+import { defineProps, useAttrs } from "vue";
+
+const props = defineProps<{
+    isLogin?: boolean;
+    isFull?: boolean;
+}>();
+const attrs = useAttrs();
+</script>
