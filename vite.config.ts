fca78b6849573ab45d3dc9f0fc0123b06f348aaa
blkhanhlinh <linhbui8103@gmail.com>
2024-05-26 14:31:59 +0700

    config: adjust some config and add gsap package

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite'
 import { fileURLToPath, URL } from 'url'
-import { resolve, dirname } from 'path'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
-    alias: [{ find: "@", replacement: resolve(__dirname, "./src") }]
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
   },
   server: {
     port: 8080
