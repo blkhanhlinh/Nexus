9e2be111929d1c81d185e6bb4a2b14541f682ed5
blkhanhlinh <linhbui8103@gmail.com>
2024-05-26 16:00:51 +0700

    feat: add 40refactor: add API URL

---
+++ src/constants/api.ts
@@ -0,0 +1,7 @@
+export const baseURL =
+    process.env.NODE_ENV === "production"
+        ? "https://truon9.id.vn/"
+        : "https://truon9.id.vn/";
+
+export const gameAPI = "https://truon9.id.vn/"
+export const userAPI = "https://truon9.id.vn/"
